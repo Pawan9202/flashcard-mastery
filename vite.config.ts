2dfe39c2e57ba4bfc7c7a19b5792f9b67313a550
Pawan Singh <135360729+Pawan9202@users.noreply.github.com>
2025-05-18 23:14:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "127.0.0.1",
     port: 8080,
   },
-  plugins: [
-    react(),
-    
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    minify: "terser",
+    sourcemap: mode === "development",
+    chunkSizeWarningLimit: 600,
+    assetsInlineLimit: 4096,
+  },
+  optimizeDeps: {
+    include: ["react", "@vitejs/plugin-react"],
   },
 }));
